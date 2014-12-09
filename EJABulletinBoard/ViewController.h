011f3d1bfc571694be09d35e80801582ef614ffd
Errol John Antonio <ejantonio@seerlabs.com>
2014-12-09 10:38:21 +0800

    Initial Commit

---
+++ EJABulletinBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EJABulletinBoard
+//
+//  Created by Errol John Antonio on 12/9/14.
+//  Copyright (c) 2014 Errol John Antonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
